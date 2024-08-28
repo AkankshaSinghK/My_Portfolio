1c9da4d6ff23c24e565083b336bbc49d926f8332
AkankshaSinghK <singhakanksha2897@gmail.com>
2024-08-28 20:02:04 +0530

    final commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from "react";
 import initializeScrollReveal from "../utils/scrollReveal";
 import profileImage from "../static/images/p1.jpeg";
-import resume from "../static/assets/Akanksha Singh - Resume.pdf";
-import TypewriterEffect from "../utils/TypewriterEffect"; 
+import resume from "../static/assets/Akanksha Singh - NewResume.pdf";
+import TypewriterEffect from "../utils/TypewriterEffect";
 
 const Home = () => {
   useEffect(() => {
     // Initialize ScrollReveal
     initializeScrollReveal();
-
   }, []);
 
-const messages = ["Frontend Developer", "MERN Developer", "FullStack Developer","Web Developer"];
+  const messages = [
+    "Frontend Developer",
+    "MERN Developer",
+    "FullStack Developer",
+    "Web Developer",
+  ];
 
   return (
     <section className="home" id="home">
@@ -19,7 +23,10 @@ const messages = ["Frontend Developer", "MERN Developer", "FullStack Developer",
         <h3>Hello, It's Me</h3>
         <h1>Akanksha Singh</h1>
         <h3>
-        And I'm a <span className="multiple-text"><TypewriterEffect textArray={messages} /></span>
+          And I'm a{" "}
+          <span className="multiple-text">
+            <TypewriterEffect textArray={messages} />
+          </span>
         </h3>
         <p>
           A fervent web development enthusiast with a deep passion for crafting
@@ -32,9 +39,6 @@ const messages = ["Frontend Developer", "MERN Developer", "FullStack Developer",
           <a href="https://github.com/AkankshaSinghK">
             <i className="bx bxl-github" />
           </a>
-          
-         
-          
         </div>
         <a href={resume} download="Akanksha Singh - Resume.pdf" className="btn">
           Download CV
